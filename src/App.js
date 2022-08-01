71e065e51453f1a190140be4a625bb64851abf33
leehua012 <gohhua12@gmail.com>
2022-08-02 01:10:56 +0800

    first UI design

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
 import './App.css';
-
+import Calculator from './components/calculatorComponent';
 function App() {
   return (
     <div className="container">
-      <div className="row">
-      <div className="display">
-        <div className="prev-operand"> 
-        </div>
-        <div className="result">
-        </div>
-      </div>
-      </div>
-      <div className="row">
-        <button className="btn btn-primary col-2">7</button>
-        <button className="btn btn-primary col-2">8</button>
-        <button className="btn btn-primary col-2">9</button>
-        <button className="btn btn-primary col-2">AC</button>
-        <button className="btn btn-primary col-2">DEL</button>
-      </div>
-      <div className="row">
-        <button className="btn btn-primary col-2">4</button>
-        <button className="btn btn-primary col-2">5</button>
-        <button className="btn btn-primary col-2">6</button>
-        <button className="btn btn-primary col-2">*</button>
-        <button className="btn btn-primary col-2">÷</button>
-      </div>
-      <div className="row">
-        <div className='col-md-6'>
-          <button className="btn btn-primary col-2">1</button>
-          <button className="btn btn-primary col-2">2</button>
-          <button className="btn btn-primary col-2">3</button>
-        </div>
-        <div className='col-md-3'>
-          <button className="btn btn-primary well">+</button>
-        </div>
-        <div className='col-md-3'>
-          <button className="btn btn-primary col-2">-</button>
-        </div>
-        <div className='row'>
-          <button className="btn btn-primary col-2">.</button>
-          <button className="btn btn-primary col-4">0</button>
-        </div>
-        <button className="btn btn-primary col-2">=</button>
-      </div>
+      <Calculator />
     </div>
   );
 }
--- src/components/calculatorComponent.js
+++ src/components/calculatorComponent.js
@@ -1,14 +1,59 @@
 import React, {Component} from "react";
-
+import './calculator.css';
 
 class Calculator extends Component {
   render() {
     return(
       <div className="container">
-        <div className="row">
-
+      <div className="row pb-0 p-3">
+        <div className="display bg-dark rounded">
+          <div className="prev-operand pt-2 text-white text-end fs-6">
+            &nbsp;0+456
+          </div>
+          <div className="result pb-1 text-white text-end fs-4">
+            0
+          </div>
         </div>
       </div>
+      <div className='row p-3'>
+      <div className="row gx-0 mb-1">
+        <button className="btn btn-primary col-2">7</button>
+        <button className="btn btn-primary col-2">8</button>
+        <button className="btn btn-primary col-2">9</button>
+        <button className="btn btn-primary col-3">AC</button>
+        <button className="btn btn-primary col-3">DEL</button>
+      </div>
+      <div className="row gx-0 mb-1">
+        <button className="btn btn-primary col-2">4</button>
+        <button className="btn btn-primary col-2">5</button>
+        <button className="btn btn-primary col-2">6</button>
+        <button className="btn btn-primary col-3">*</button>
+        <button className="btn btn-primary col-3">÷</button>
+      </div>
+      <div className="row gx-0">
+        <div className='col-6'>
+          <div className="col-12 mb-1">
+            <button className="btn btn-primary col-4">1</button>
+            <button className="btn btn-primary col-4">2</button>
+            <button className="btn btn-primary col-4">3</button>
+          </div>
+          <div className="col-12">
+            <button className="btn btn-primary col-6">.</button>
+            <button className="btn btn-primary col-6">0</button>
+          </div>
+        </div>
+        <div className='col-3'>
+          <button className="btn btn-primary col-12">
+            <div className='col-12'>&nbsp;<br/>+<br/>&nbsp;</div>
+            </button>
+        </div>
+        <div className='col-3'>
+          <button className="btn btn-primary col-12 mb-1">-</button>
+          <button className="btn btn-primary col-12 ">=</button>
+        </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
