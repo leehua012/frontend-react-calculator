0240289ab176561369b7ca2800592a14cc0a7c6e
leehua012 <gohhua12@gmail.com>
2022-08-01 22:12:20 +0800

    second commit

---
+++ src/components/calculatorComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+
+class Calculator extends Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Calculator;
