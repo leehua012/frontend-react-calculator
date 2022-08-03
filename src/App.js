ab2fafb144f513dbfc56818ea6ca48646d880fa8
leehua012 <gohhua12@gmail.com>
2022-08-03 20:28:19 +0800

    color adjustment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Calculator from './components/calculatorComponent';
 function App() {
   return (
     <div className="container">
-      <div className='h1 mb-4 text-center'>Calculator</div>
+      <div className='h1 mb-4 text-center tf'> My Calculator</div>
       <Calculator />
     </div>
   );
